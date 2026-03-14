31cd93d3521795c2d6df8f37776936779920e0c2
suharrismd-suh <suharrismd@gmail.com>
2026-03-14 14:16:49 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_API_KEY",
-  authDomain: "REPLACE_PROJECT_ID.firebaseapp.com",
-  projectId: "REPLACE_PROJECT_ID",
-  storageBucket: "REPLACE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "REPLACE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_APP_ID",
+  apiKey: "AIzaSyD6vsX8wopEnADtUakLRaw0ReZXpyt7dN0",
+  authDomain: "suh-payroll-tracker.firebaseapp.com",
+  projectId: "suh-payroll-tracker",
+  storageBucket: "suh-payroll-tracker.firebasestorage.app",
+  messagingSenderId: "499356095206",
+  appId: "1:499356095206:web:279990d4d084e9db532127",
 };
 
 export const defaultWorkspaceId = "corina-elgart-timesheet";
